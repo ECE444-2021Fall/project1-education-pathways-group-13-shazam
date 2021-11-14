d632f4e8047805b4f1d045f81ff3402e3c9ee30d
Yanchen Ma <yanchenm@gmail.com>
2021-11-14 18:41:48 -0500

    working login

--- pathfinder-frontend/lib/auth/useUser.js
+++ pathfinder-frontend/lib/auth/useUser.js
@@ -5,7 +5,7 @@ import useSWR from 'swr';
 
 // Pattern taken from https://github.com/vvo/iron-session/blob/main/examples/next.js/lib/useUser.js
 const useUser = (redirectTo = '', redirectIfFound = false) => {
-  const { data: user, mutate: mutateUser } = useSWR('/user', fetcher);
+  const { data: user, mutate: mutateUser } = useSWR('/user/', fetcher);
 
   useEffect(() => {
     // If user not found yet, or there is no redirect, do nothing
