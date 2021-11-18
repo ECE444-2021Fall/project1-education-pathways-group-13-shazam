a8539e1ec37fff758165343c168db1729adf004d
Daniel Liang <daniel65@live.ca>
2021-11-17 23:58:10 -0500

    Put NavBar on landing page, change buttons depending on logged in or not

--- pathfinder-frontend/pages/index.js
+++ pathfinder-frontend/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 import styles from '../styles/Home.module.css';
+import NavBar from '../components/navbar';
 
 export default function Home() {
   return (
@@ -10,17 +11,7 @@ export default function Home() {
         <title>PathFinder</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={styles.topbar}>
-        <div className={styles.appname}>PathFinder</div>
-        <div className={styles.login}>
-          <Link href="/login">
-            <a>Log In</a>
-          </Link>
-          <Link href="/signup">
-            <a>Sign Up</a>
-          </Link>
-        </div>
-      </div>
+      <NavBar />
       <main className={styles.main}>
         <div id={styles.earth}>
           <Image src="/earth.svg" alt="earth" width={153} height={130} />
@@ -49,8 +40,8 @@ export default function Home() {
             <h1>Selecting courses is hard...</h1>
             <br />
             <p>
-              Between checking graduation eligibility, balancing workloads and checking prequisites; course selection can
-              feel like rocket science.
+              Between checking graduation eligibility, balancing workloads and checking prequisites; course selection
+              can feel like rocket science.
             </p>
             <br />
             <p>Use PathFinder and save your time and energy for the important things in life!</p>
